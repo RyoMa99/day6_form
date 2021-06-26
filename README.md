237cc54a236247c2501cbab53daeafe058884c5f
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-06-26 14:38:19 +0000

    fin

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 # day6
+[vue form 色々](https://reffect.co.jp/vue/vue-js-input-operate#i-9)
+
+## 学んだこと
+- チェックボックスは真偽値以外もデータを持たせられる。
+  ```js
+  <input
+    v-model="checkValue"
+    type="checkbox"
+    true-value="はい"
+    false-value="いいえ"
+  >
+  ```
+- 配列にvalueが存在するかどうかの真偽値で判断することも出来る
+  ```js
+  <input
+    v-model="checkValues"
+    type="checkbox"
+    value="ラグビー"
+  > ラグビー
+  <input
+    v-model="checkValues"
+    type="checkbox"
+    value="サッカー"
+  > サッカー
+  <input
+    v-model="checkValues"
+    type="checkbox"
+    value="バスケットボール"
+  > バスケットボール
+  ```
+- セレクトボックスはdisabledを使ってプレースホルダーの様な役割を与えられる
+  ```js
+  <option
+    disabled
+    value=""
+  >
+    興味のあるスポーツを選択
+  </option>
+  ```
+- Event型はinterfaceでtargetがない。`event.target instanceof HTMLInputElement`を用いて型ガードを通す必要がある。
+  (参考)[https://yuutookun.hatenablog.com/entry/2018/07/21/084912]
 
 ## Project setup
 ```
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,6 +1,90 @@
 <template>
   <div>
-
+    <p>{{ textInput }}</p>
+    <!-- テキスト -->
+    <input
+      v-model="textInput"
+      type="text"
+    >
+    <!-- テキストエリア -->
+    <textarea v-model="textInput" />
+    <p>{{ checkValue }}</p>
+    <!-- 真偽値以外のチェックボックス -->
+    <input
+      v-model="checkValue"
+      type="checkbox"
+      true-value="はい"
+      false-value="いいえ"
+    >
+    <!-- チェックボックス配列 -->
+    <p>{{ checkValues }}</p>
+    <input
+      v-model="checkValues"
+      type="checkbox"
+      value="ラグビー"
+    > ラグビー
+    <input
+      v-model="checkValues"
+      type="checkbox"
+      value="サッカー"
+    > サッカー
+    <input
+      v-model="checkValues"
+      type="checkbox"
+      value="バスケットボール"
+    > バスケットボール
+    <!-- ラジオボタン -->
+    <p>{{ radioValue }}</p>
+    <input
+      v-model="radioValue"
+      type="radio"
+      value="ラグビー"
+    > ラグビー
+    <input
+      v-model="radioValue"
+      type="radio"
+      value="サッカー"
+    > サッカー
+    <input
+      v-model="radioValue"
+      type="radio"
+      value="バスケットボール"
+    > バスケットボール
+    <!-- セレクトボックス -->
+    <p>{{ selectValue }}</p>
+    <select v-model="selectValue">
+      <option
+        disabled
+        value=""
+      >
+        興味のあるスポーツを選択
+      </option>
+      <option>値1</option>
+      <option>値2</option>
+      <option>値3</option>
+    </select>
+    <!-- セレクトボックス 複数 -->
+    <p>{{ selectValues }}</p>
+    <select
+      v-model="selectValues"
+      multiple=""
+    >
+      <option
+        disabled
+        value=""
+      >
+        興味のあるスポーツを選択
+      </option>
+      <option>値1</option>
+      <option>値2</option>
+      <option>値3</option>
+    </select>
+    <!-- ファイルオブジェクト -->
+    <p>{{ fileData.name }}</p>
+    <input
+      type="file"
+      @change="handleFile"
+    >
   </div>
 </template>
 
@@ -9,7 +93,20 @@ import { Vue, Component } from 'vue-property-decorator'
 
 @Component({})
 export default class extends Vue {
+  textInput = ''
+  checkValue = 'いいえ'
+  checkValues = []
+  radioValue = ''
+  selectValue = ''
+  selectValues = []
+  fileData = new File([], '')
 
+  handleFile (event: InputEvent): void {
+    if (!(event.target instanceof HTMLInputElement)) return
+    if (event.target.files === null) return
+    const file = event.target.files[0]
+    this.fileData = file
+  }
 }
 </script>
 
